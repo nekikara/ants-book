653b77ae747603e7a837951c82150626da84939b
nekikara <a.nek1427@gmail.com>
2015-06-13 21:46:41 +0900

    ナップザックのDPの対象を変更したバージョンを追加

--- chapter_2/napsack.rb
+++ chapter_2/napsack.rb
@@ -9,6 +9,7 @@ $W = 5
 
 $dp = Array.new($n + 1) { Array.new($W + 1, 0) }
 
+# 重さのDP
 (1..$n).each do |y|
   (1..$W).each do |x|
     w = $w[y - 1]
@@ -20,4 +21,38 @@ $dp = Array.new($n + 1) { Array.new($W + 1, 0) }
   end
 end
 
-pp $dp
+#puts '#1'
+#pp $dp
+
+MAX_N = 4
+MAX_V = 10
+$dp = Array.new($n + 1) { Array.new(MAX_N * MAX_V + 1, nil) }
+(0..(MAX_N * MAX_V + 1)).each do |i|
+  $dp[0][i] = 10000
+end
+(1..$n).each do |i|
+  $dp[i][0] = 10000
+end
+$dp[0][0] = 0
+
+# 価値のDP
+(1..$n).each do |y|
+  (1..(MAX_N * MAX_V)).each do |x|
+    v = $v[y - 1]
+    if x < v
+      $dp[y][x] = $dp[y - 1][x]
+    else
+      candidates = [$dp[y - 1][x]]
+      candidates << ($dp[y - 1][x - $v[y - 1]] + $w[y - 1])
+      $dp[y][x] = candidates.min
+    end
+  end
+end
+
+puts '#2'
+i = 0
+res = 0
+(1..(MAX_N * MAX_V)).each do |i|
+  res = i if $dp[$n][i] <= $W
+end
+pp res
